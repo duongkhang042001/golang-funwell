e5cd8c5d9b8693a9608923a49f4f772a30c5d309
Khang Duong <khangdt10@fpt.com>
2024-09-10 11:02:35 +0700

    feat(user): register user route to bootstrap

--- internal/infrastructure/server/bootstrap.go
+++ internal/infrastructure/server/bootstrap.go
@@ -8,6 +8,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 
 	apiMiddlewares "core/internal/interfaces/http/middleware"
+	"core/internal/interfaces/http/route"
 )
 
 func (app *Application) Bootstrap(e *echo.Echo) error {
@@ -26,6 +27,8 @@ func (app *Application) Bootstrap(e *echo.Echo) error {
 	}
 
 	v1 := e.Group("/api/v1")
+
+	route.MapAuthRoutes(v1.Group("/auth"))
 
 	health := v1.Group("/health")
 
--- internal/interfaces/http/route/auth.go
+++ internal/interfaces/http/route/auth.go
@@ -10,6 +10,10 @@ func MapAuthRoutes(router *echo.Group) {
 	router.POST("/sign-in", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Hello, Sign in page!")
 	})
+
+	router.GET("/sign-in", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "Hello, Sign in page!")
+	})
 	router.POST("/sign-up", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Hello, Sign up page!")
 	})
