7b1615c84adbe13172ac131bfd3c867897f40e40
Khang Duong <duongtrongkhang042001@outlook.com>
2025-02-01 19:57:54 +0700

    feat(worker): implement graceful shutdown for worker process

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"core/config"
 	"core/pkg/logger"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -21,21 +25,24 @@ func main() {
 
 	logger.Infof("Worker is running")
 
-	// ctx, cancel := context.WithCancel(context.Background())
+	_, cancel := context.WithCancel(context.Background())
 
-	// terminateSignals := make(chan os.Signal, 1)
+	terminateSignals := make(chan os.Signal, 1)
 
-	// signal.Notify(terminateSignals, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+	signal.Notify(terminateSignals, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
 
-	// stop := false
-	// for !stop {
-	// 	select {
-	// 	case s := <-terminateSignals:
-	// 		logger.Info("Got one of stop signals, shutting down worker gracefully, SIGNAL NAME :", s)
-	// 		cancel()
-	// 		stop = true
-	// 	}
-	// }
+	stop := false
+	for !stop {
+		select {
+		case s := <-terminateSignals:
+			logger.Info("Got one of stop signals, shutting down worker gracefully, SIGNAL NAME :", s)
+			cancel()
+			stop = true
+		default:
+			time.Sleep(1 * time.Second)
+		}
+		
+	}
 
 	time.Sleep(5 * time.Second) // wait for all consumers to finish processing
 }
